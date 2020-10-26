1fea16f319fd9f888755ba4cdd7277c2672110a2
Lamelie <ameliederoche@hotmail.com>
2020-10-26 18:45:51 +0100

    Suppression temporaire de la fonction de Captcha

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 require('bootstrap');
-require('./captcha');
+//require('./captcha');
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.scss';
@@ -17,24 +17,23 @@ const axios = require('axios/dist/axios');
 
 let $collectionHolder;
 
-// setup an "add a step" link
+// ajoute un lien "ajouter une étape"
 const $addStepButton = $('<button type="button" class="btn tutodeco-btn">Ajouter une étape</button>');
 const $newLinkLi = $('<li></li>').append($addStepButton);
 
 $(document).ready(function() {
-    // Get the ul that holds the collection of steps
+    // Récupère la collection de "steps"
     $collectionHolder = $('ul.steps');
 
-    // add a delete link to all of the existing step form li elements
+    // ajoute un lien "supprimer" aux étapes existantes
     $collectionHolder.find('li').each(function() {
         addStepFormDeleteLink($(this));
     });
 
-    // add the "add a step" anchor and li to the steps ul
+    // ajoute un bouton "ajouter un étape" après les étapes existantes
     $collectionHolder.append($newLinkLi);
 
-    // count the current form inputs we have (e.g. 2), use that as the new
-    // index when inserting a new item (e.g. 2)
+    // compte le nombre d'étapes et ajoute ce nombre à la data "index"
     $collectionHolder.data('index', $collectionHolder.find('textarea').length);
 
     $addStepButton.on('click', function(e) {
@@ -51,11 +50,11 @@ function addStepForm($collectionHolder, $newLinkLi) {
 
     let newForm = prototype;
 
-    // Replace '__name__' in the prototype's HTML to
-    // instead be a number based on how many items we have
+    // remplace '__name__' dans le prototype HTML
+    // au lieu d'avoir le nombre de l'index
     newForm = newForm.replace(/__name__/g, index);
 
-    // increase the index with one for the next item
+    // augmente l'index de un pour la prochaine étape
     $collectionHolder.data('index', index + 1);
 
     // affiche le formulaire de création d'étape avant le bouton "ajouter une étape"
@@ -92,9 +91,8 @@ $(document).ready(function() {
     });
 })
 
-//TODO : mettre le select2 pour les tags.
 
-//permet l'ajout en done-list
+//permet l'ajout en liste des tutoriels réalisés
 $(document).ready(function () {
 
     function onClickBtnDone(event) {
@@ -105,6 +103,7 @@ $(document).ready(function () {
         const icone = $('i', this)
         console.log(icone);
 
+        //récupère les données envoyées en Json au clic sur le bouton.
         axios.get(url).then(function (response) {
             const dones = response.data.dones;
             nbDone.text(dones);
@@ -135,10 +134,14 @@ $(document).ready(function () {
         const nbTodo = $('.js-nbTodo', this)
         const icone = $('i', this)
 
+        //récupère les données envoyées en Json au clic sur le bouton.
         axios.get(url).then(function (response) {
+            //récupère le nombre de todos
             const todos = response.data.todos;
+            //affiche le nombre de todos
             nbTodo.text(todos);
 
+            //remplace les icones vides/pleines en fonction de l'ajout ou suppression
             if(icone.hasClass('fas')){
                 icone.removeClass('fas fa-clipboard-list').addClass('far fa-clipboard')
             } else {
@@ -150,6 +153,7 @@ $(document).ready(function () {
             }
         })
     }
+    //applique la fonction au clic sur l'icone.
     const $todoLinks = $('a.js-todo-link')
     $todoLinks.on('click', onClickBtnTodo);
 })
